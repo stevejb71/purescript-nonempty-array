926a2ef0e00e7f2e8bcd32320f190d423160a5f4
stevejb71 <stevejb71@yahoo.co.uk>
2014-11-16 14:52:54 +0800

    start testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,19 @@ module.exports = function(grunt) {
       "src/**/*.purs",
       "bower_components/*/src/**/*.purs",
     ],
-    clean: ["output"],
-    pscMake: ["<%=libFiles%>"],
+    clean: ["output", "output_tests"],
+    pscMake: {
+      src: ["<%=libFiles%>"],
+      tests: {
+        src: ["tests/Main.purs", "<%=libFiles%>"]
+      }
+    },
+    psc: {
+      tests: {
+        src: ["tests/Main.purs", "<%=libFiles%>"],
+        dest: "output_tests/Main.js"
+      }
+    },
     dotPsci: ["<%=libFiles%>"],
     docgen: {
         readme: {
@@ -15,19 +26,18 @@ module.exports = function(grunt) {
             dest: "README.md"
         }
     },
-    tests: {
-        options: {
-          module: ["Main"],
-          main: true
-        },
-        src: ["tests/Main.purs", "<%=srcFiles%>"],
-        dest: "dist/tests.js"
-    }
+    execute: {
+      tests: {
+        src: ["output_tests/Main.js"]
+      }
+    } 
   });
 
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-purescript");
+  grunt.loadNpmTasks("grunt-execute");
   
   grunt.registerTask("make", ["pscMake", "dotPsci", "docgen"]);
-  grunt.registerTask("default", ["make"]);
+  grunt.registerTask("test", ["psc", "execute:tests"]);
+  grunt.registerTask("default", ["clean", "make", "test"]);
 };
--- tests/Main.purs
+++ tests/Main.purs
@@ -1,4 +1,13 @@
 module Main where
 
+import Test.QuickCheck
+import qualified Data.Array as A
+import Data.Array.NonEmpty
+
+foreign import undefined :: forall a. a
+
+instance arbNonEmpty :: (Arbitrary a) => Arbitrary (NonEmpty a) where
+  arbitrary = undefined
+
 main = do
-    quickCheck $ \xs ys -> isSorted $ merge (sort xs) (sort ys)
+    quickCheck \xs -> length xs == A.length (toArray xs)
