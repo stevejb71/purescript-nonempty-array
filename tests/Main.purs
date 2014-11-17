c2a863e1e54a27c728ecef35809a23f033002de5
stevejb71 <stevejb71@yahoo.co.uk>
2014-11-17 14:39:23 +0800

    more tests and no need for fromJust in tests

--- tests/Main.purs
+++ tests/Main.purs
@@ -3,10 +3,8 @@ module Main where
 import Test.QuickCheck
 import qualified Data.Array as A
 import Data.Array.NonEmpty
-import Debug.Trace
-import Data.Maybe.Unsafe
-
-foreign import undefined :: forall a. a
+import Data.Array.NonEmpty.Unsafe
+import Data.Maybe   
 
 instance arbNonEmpty :: (Arbitrary a) => Arbitrary (NonEmpty a) where
   arbitrary = NonEmpty <$> arbitrary <*> arbitrary
@@ -31,17 +29,19 @@ main = do
     qc "Inequality" \x -> x /= push 0 x
     qc "Length" \x -> length x == A.length (toArray x)
     qc "Pop - Length" \x -> A.length (pop x) == A.length (toArray x) - 1
-    qc "Pop - Head" \(NonEmpty a as) -> let popped = pop (NonEmpty a as) in A.length popped == 0 || (fromJust $ A.head popped) == a
+    qc "Pop - Head" \(NonEmpty a as) -> let popped = pop (NonEmpty a as) in A.length popped == 0 || A.head popped == Just a
     qc "Take" \xs -> take (length xs) xs == toArray xs
+    qc "Take 1" \xs -> A.head (take 1 xs) == Just (head xs)
     qc "Drop" \xs -> A.length (drop (length xs) xs) == 0
     qc "Drop 1" \xs -> drop 1 xs == tail xs
     qc "Reverse" \xs -> reverse (reverse xs) == xs
+    qc "Reverse - Last" \xs -> last (reverse xs) == head xs
     qc_ll "Append - Length" \xs ys -> length (xs `append` ys) == length xs + length ys
     qc_nl "Cons - Length" \x xs -> length (x <| xs) == length xs + 1
     qc_nl "Cons - Head" \x xs -> head (x <| xs) == x
     qc_nl "Cons - Tail" \x xs -> tail (x <| xs) == toArray xs
-    qc "Last" \(NonEmpty a as) -> let x = (NonEmpty a as) in (A.length as == 0 && last x == a) || (last x == fromJust (A.last as))
-    qc "nub" \xs -> xs == nub (xs `append` xs)
+    qc "Last" \(NonEmpty a as) -> let x = (NonEmpty a as) in (A.length as == 0 && last x == a) || (Just (last x) == A.last as)
     qc "nubBy" \xs -> xs == nubBy (==) (xs `append` xs)
     qc "functor - identity" \xs -> (\x -> x) <$> xs == xs 
     qc "functor - composition" \xs -> ((+) 1) <$> ((+) 2) <$> xs == ((+) 3) <$> xs
+    qc "fromArray" \(NonEmpty a as) -> fromArray (a:as) == (NonEmpty a as)
